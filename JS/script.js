dd94d96d77b81f101f7fef4fc62dc93b9b529ff3
TiennLevii <nguyentrachtien2401@gmail.com>
2023-07-02 22:37:25 +0700

    Data

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,4 @@
-const API = "https://testapi-tj1g.onrender.com/List/";
+const API = "https://testapi-tj1g.onrender.com";
 
-function currentAPI() {
-    fetch(API)
-        .then(response => response.json())
-        .then(data => {
-            const list = data.map(dataList => {
-                return `<div class="movie-content">
-                <img src="${dataList.img}">
-                <h1>${dataList.name}</h1>
-                <div class="play-icon">
-                <a href="detail.html?id=${dataList.id}"><i class="fa-sharp fa-solid fa-play"></i></a>
-                </div>
-                </div>`
-            })
-            document.querySelector(".movies").innerHTML = list.join("");
-        });
-}
-
-function getAPI() {
-    fetch(API)
-        .then(response => response.json())
-        .then(response => console.log(response))
-}
-getAPI()
-currentAPI()
+fetch(API).then(response => response.json())
+    .then(response => console.log(response));
