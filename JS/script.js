41beae4e2b98a34a53ea654b40f6081246f5ad3e
TiennLevii <nguyentrachtien2401@gmail.com>
2023-07-02 22:39:17 +0700

    Data

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,9 @@
 const API = "https://testapi-tj1g.onrender.com";
 
-fetch(API).then(response => response.json())
-    .then(response => console.log(response));
+function getAPI() {
+    fetch(API)
+        .then(response => response.json())
+        .then(response => console.log(response))
+}
+
+getAPI();
